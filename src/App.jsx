d78c7a6c41913f3bb56cb20f35481cfb78231974
rohan <rohan@rohans-MacBook-Air-2.local>
2023-06-20 21:44:03 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React, { Suspense,useRef } from "react";
+import React, { Suspense,useRef, useState,  useEffect} from "react";
 import { Canvas,useFrame } from "@react-three/fiber";
 import {OrbitControls,useGLTF} from "@react-three/drei";
 import './App.css';
+import Clock from "./Components/Clock";
+
+ 
  function Model(props) {
   const { nodes, materials } = useGLTF('/model.glb')
+ 
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.computer_computer_mat_0.geometry} material={materials.computer_mat} />
@@ -29,8 +33,56 @@ import './App.css';
 
 
 export default function App() {
+  const[timerDays, setTimerDays] = useState();
+  const[timerHours, setTimerHours] = useState();
+  const[timerMinutes, setTimerMinutes] = useState();
+  const[timerSeconds, setTimerSeconds] = useState();
+  
+  let interval;
+
+  const startTimer=()=>{
+    const countDownDate= new Date("June 30,2023ð").getTime();
+
+    interval=setInterval(()=>{
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days= Math.floor(distance/(24 * 60 * 60 * 1000));
+
+      const hours= Math.floor((distance % (24 * 60 * 60 * 1000))/(1000*60*60));
+
+      const minutes= Math.floor((distance % (60 * 60 * 1000))/(1000*60));
+
+      const seconds= Math.floor((distance % (60 * 1000))/(1000));
+
+      if(distance<0){
+        // stop timer
+
+        clearInterval(interval.current);
+      }
+      else{
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(()=> {
+    startTimer();
+  });
+
   return (
     <div className="wrapper" style={{ width: "100vw", height: "100vh" }}>
+      
+      <Clock 
+      timerDays={timerDays} 
+      timerHours={timerHours} 
+      timerMinutes={timerMinutes} 
+      timerSeconds={timerSeconds}/>
+
       <Canvas camera={{fov:100,position:[-1,1,4]}}>
       <OrbitControls enablePan={true} enableZoom={true}/>
         <Suspense fallback={null}>
@@ -41,6 +93,8 @@ export default function App() {
         </Suspense>
      
       </Canvas>
+
+      
     </div>
   );
 }
---
+++ src/Components/Clock.jsx
@@ -0,0 +1,48 @@
+import React,{ Fragment } from 'react'
+
+const Clock = ({timerDays, timerHours, timerMinutes, timerSeconds}) => {
+  return (
+   <Fragment>
+    <div className="timer-container">
+      <div className="timer">
+        <div className="clock">
+
+          <section className='he'>
+            <p>{timerDays}</p>
+            <small>Days</small>
+          </section>
+          <span>:</span>
+          
+
+          <section className='he'>
+            <p> { timerHours}</p>
+            <small> Hours </small>
+          </section>
+          <span>:</span>
+          
+
+          <section className='he'>
+            <p>{ timerMinutes }</p>
+            <small>Minutes</small>
+          </section>
+          <span>:</span>
+          
+
+          <section className='he'>
+            <p>{timerSeconds}</p>
+            <small>Seconds</small>
+          </section>
+        </div>
+      </div>
+    </div>
+   </Fragment>
+  )
+}
+
+Clock.defaultProps = {
+  timerDays: 10,
+  timerHours: 10,
+  timerMinutes: 10,
+  timerSeconds: 10
+}
+export default Clock
